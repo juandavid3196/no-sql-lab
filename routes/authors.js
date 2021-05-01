623b7d07d96bae8e997a3d4e546260f081e135dd
juandavid3196 <juandavid3196@gmail.com>
2021-05-01 18:10:55 -0500

    se agregaron los Endpoints

--- routes/authors.js
+++ routes/authors.js
@@ -30,4 +30,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Consultar los nombres y los apellidos de los autores que tengan publicaciones superiores o iguales a 20 y su país sea Colombia.
+ 
+ router.get('/consulta1', async (req, res) => {
+  try {
+    let f = {pais: "Colombia", publicados:{$gt:20}};
+    let p = {nombre:1, apellido:1, _id:0};
+    const authors = await Author.find(f, p);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+//Consultar los nombres de los autores que no tengan apellido en la base de datos.
+ 
+router.get('/consulta2', async (req, res) => {
+  try {
+    let f = {apellido: null};
+    let p = {nombre:1, _id:0};
+    const authors = await Author.find(f, p);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Consultar los apellidos de los autores que tengan menos de 20 publicaciones o que su país sea Argentina.
+ 
+router.get('/consulta3', async (req, res) => {
+  try {
+    let f ={$or:[{publicados:{$lt:20}},{pais: "Argentina"}]};
+    let p = {apellido:1, _id:0};
+    const authors = await Author.find(f, p);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
